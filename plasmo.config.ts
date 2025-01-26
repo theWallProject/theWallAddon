dd73532857a2aee8e385de39f15e018c29bd096e
theWall <the.watermelon.project1@gmail.com>
2025-01-26 02:53:53 +0100

    remove extra files

--- plasmo.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite"
-// @ts-expect-error
-import gltf from "vite-plugin-gltf"
-
-throw new Error("This is a test error")
-export default defineConfig({
-  plugins: [gltf()], // Add the GLTF plugin
-  assetsInclude: ["**/*.gltf", "**/*.glb"] // Ensure Vite handles .gltf and .glb as assets
-})
